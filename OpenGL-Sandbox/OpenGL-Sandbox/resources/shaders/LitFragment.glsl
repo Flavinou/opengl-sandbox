be383b3114b45a5d31a8386c0d889b78e6269499
Flavinou <flafebvay@gmail.com>
2025-06-13 16:01:58 +0200

    Implement Gouraud shading
    Shading and lighting calculation is done per vertex in the vertex shader -> less calculations as there are usually less vertices than fragments but the visual effect on specular highlights seems off

--- OpenGL-Sandbox/OpenGL-Sandbox/resources/shaders/LitFragment.glsl
+++ OpenGL-Sandbox/OpenGL-Sandbox/resources/shaders/LitFragment.glsl
@@ -1,31 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 FragmentPosition;
-in vec3 Normal;
-in vec3 LightViewPos;
+in vec4 LightingColor; // This will be set by the vertex shader or application code
 
-uniform vec4 u_LightColor;
 uniform vec4 u_TintColor;
 
 void main()
 {
-	float ambientStrength = 0.1;
-	vec4 ambient = ambientStrength * u_LightColor;
-
-	vec3 norm = normalize(Normal);
-	vec3 lightDirection = normalize(LightViewPos - FragmentPosition);
-
-	float diff = max(dot(norm, lightDirection), 0.0);
-	vec4 diffuse = diff * u_LightColor;
-
-	float specularStrength = 0.5;
-	vec3 viewDirection = normalize(-FragmentPosition); // The viewer is always at the origin in view-space
-	vec3 reflectionDirection = reflect(-lightDirection, norm);
-
-	float spec = pow(max(dot(viewDirection, reflectionDirection), 0.0), 32);
-	vec4 specular = specularStrength * spec * u_LightColor;
-
-	vec4 result = (ambient + diffuse + specular) * u_TintColor;
-	FragColor = result;
+	FragColor = LightingColor * u_TintColor;
 }
--- OpenGL-Sandbox/OpenGL-Sandbox/resources/shaders/Vertex.glsl
+++ OpenGL-Sandbox/OpenGL-Sandbox/resources/shaders/Vertex.glsl
@@ -5,6 +5,7 @@ layout (location = 1) in vec3 aNormal; // Vertex normal
 out vec3 FragmentPosition;
 out vec3 Normal;
 out vec3 LightViewPos; // Light position in view space
+out vec4 LightingColor; // Vertex color for Gouraud shading
 
 uniform vec3 u_LightPosition; // Light position in world space
 
@@ -12,10 +13,31 @@ uniform mat4 u_Model;
 uniform mat4 u_View;
 uniform mat4 u_Projection;
 
+uniform vec4 u_LightColor;
+
 void main()
 {
 	gl_Position = u_Projection * u_View * u_Model * vec4(aPos, 1.0);
 	FragmentPosition = vec3(u_View * u_Model * vec4(aPos, 1.0));
 	Normal = mat3(transpose(inverse(u_View * u_Model))) * aNormal;
 	LightViewPos = vec3(u_View * vec4(u_LightPosition, 1.0)); // Transform light position to view space
+
+	// Implement Gouraud shading
+	float ambientStrength = 0.1;
+	vec4 ambient = ambientStrength * u_LightColor;
+
+	vec3 norm = normalize(Normal);
+	vec3 lightDirection = normalize(LightViewPos - FragmentPosition);
+
+	float diff = max(dot(norm, lightDirection), 0.0);
+	vec4 diffuse = diff * u_LightColor;
+
+	float specularStrength = 1.0;
+	vec3 viewDirection = normalize(-FragmentPosition); // The viewer is always at the origin in view-space
+	vec3 reflectionDirection = reflect(-lightDirection, norm);
+
+	float spec = pow(max(dot(viewDirection, reflectionDirection), 0.0), 32);
+	vec4 specular = specularStrength * spec * u_LightColor;
+
+	LightingColor = ambient + diffuse + specular;
 }
